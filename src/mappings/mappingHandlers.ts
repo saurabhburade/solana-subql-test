44b536705d140c1f3d2b57bb021f0aa7b87cdd98
Saurabh Burade <sburade3@gmail.com>
2025-05-28 16:25:47 +0530

    fix: liq instruction decode

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,12 +2,11 @@ import assert from "node:assert";
 
 import { SolanaInstruction } from "@subql/types-solana";
 import { TransactionForFullJson } from "@solana/kit";
-import { TokenAccount, Transfer } from "../types/models";
+import { AddLiquidity, TokenAccount, Transfer } from "../types/models";
 import {
   AddLiquidityInstruction,
-  AddLiquidityOneSideInput,
   AddLiquidityOneSideInstruction,
-} from "../types/program-interfaces/LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo";
+} from "../types/handler-inputs/LBUZKhRxPF3XUpBCjp4YzTKgLccjZhTSDM9YuVaPwxo";
 import { BlockFilter, LogFilter } from "@subql/common-solana";
 
 function bnReplacer(value: any): any {
@@ -37,7 +36,19 @@ export function getAccountByIndex(
 export async function handleLiquidity(
   instruction: AddLiquidityOneSideInstruction
 ) {
-  logger.info("handleLiquidity : programAddress : {}", [
-    instruction.programAddress.toString(),
-  ]);
+  const decoded = await instruction.decodedData;
+  if (decoded) {
+    const accounts = instruction.accounts.map((account) => account.toString());
+    logger.info("handleLiquidity : programAddress : {}", [decoded.name]);
+    const newLiq = AddLiquidity.create({
+      id: `${instruction.transaction.transaction.signatures[0]}-${instruction.index.join(".")}`,
+      accounts: JSON.stringify(accounts),
+      blockNumber: instruction.block.blockHeight,
+      data: JSON.stringify(decoded?.data.liquidityParameter),
+      program: decoded.name,
+      transactionHash: instruction.transaction.transaction.signatures[0],
+    });
+
+    await newLiq.save();
+  }
 }
